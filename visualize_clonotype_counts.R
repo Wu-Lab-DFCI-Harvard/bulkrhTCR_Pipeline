bad412dda08e4b7d5411f5ac924066e8e346d1ab
Bohoon Shim <53489568+bshim181@users.noreply.github.com>
2024-02-12 13:42:01 -0500

    Update visualize_clonotype_counts.R

--- visualize_clonotype_counts.R
+++ visualize_clonotype_counts.R
@@ -1,6 +1,3 @@
-#install.packages('ggplot2')
-#install.packages("tidyverse")
-
 library(ggplot2)
 library(tidyverse)
 
@@ -13,40 +10,34 @@ sample_name <- args[4]
 TRA <- data.table::fread(TRA_path)
 TRB <- data.table::fread(TRB_path)
 
-TRA_data <- data.frame(vGene=sort(unique(TRA$vGene)))
-umi_count <- c()
-for (gene in sort(unique(TRA$vGene))) {
-  umi_count <- c(umi_count, sum(TRA[TRA$vGene == gene,]$totalUMICount))
-}
-TRA_data$value <- umi_count
+Total_TRA <- paste("Total Number of Unique Clonotypes:",TRA$Unique_Clonotypes[1])
+Total_TRB <- paste("Total Number of Unique Clonotypes:",TRB$Unique_Clonotypes[1])
 
-TRB_data <- data.frame(vGene=sort(unique(TRB$vGene)))
-umi_count <- c()
-for (gene in sort(unique(TRB$vGene))) {
-  umi_count <- c(umi_count, sum(TRB[TRB$vGene == gene,]$totalUMICount))
-}
-TRB_data$value <- umi_count
+TRA_data <- data.frame(table(TRA$vGene))
+TRB_data <- data.frame(table(TRB$vGene))
 
 
-p_a <- ggplot(TRA_data, aes(x=vGene, y=value)) +
-    ggtitle("Unique Clonotypes for each TRAV Genes") +
+p_a <- ggplot(TRA_data, aes(x=Var1, y=Freq)) +
+    labs(title = "Unique Clonotypes for each TRAV Genes", subtitle = Total_TRA) + 
+    #ggtitle(paste("Unique Clonotypes for each TRAV Genes /", Total_TRA)) +
     geom_bar(position='dodge',stat='identity', fill='steelblue') + 
     ylab("Number of Unique Clonotypes") + 
     xlab("Productive V Genes") + 
     theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, face="bold"))
 
 TRA_sample <- paste("/",sample_name,"_TRAV.png", sep="")
 TRA_output <- paste(outdir, TRA_sample, sep="")
 ggsave(TRA_output,plot=p_a, width = 30, height = 20, units = "cm", bg='white')
 
-p_b <- ggplot(TRB_data, aes(x=vGene, y=value)) +
-    ggtitle("Unique Clonotypes for each TRBV Genes") +
+p_b <- ggplot(TRB_data, aes(x=Var1, y=Freq)) +
+    labs(title = "Unique Clonotypes for each TRBV Genes", subtitle = Total_TRB) + 
+    #ggtitle(paste("Unique Clonotypes for each TRBV Genes / ",Total_TRB)) +
     geom_bar(position='dodge',stat='identity', fill='steelblue') + 
     ylab("Number of Unique Clonotypes") + 
     xlab("Productive V Genes") + 
     theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, face="bold"))
 
 TRB_sample <- paste("/", sample_name,"_TRBV.png",sep="")
 TRB_output <- paste(outdir, TRB_sample,sep="")
